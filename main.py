0f1ed619bd92f906a19f732772eff70966cf761b
Sukrit <56361047+sukritkapil2@users.noreply.github.com>
2020-10-26 21:21:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,37 @@ print('Welcome to Selective Topics Assignment')
 
 model_info = {}
 
-# # Building 12 models
-models = [keras_models.model_dense_1, keras_models.model_dense_2, keras_models.model_dense_3, keras_models.model_dense_4,
-    keras_models.cnn_model_5, keras_models.cnn_model_6, keras_models.cnn_model_7, keras_models.cnn_model_8,
-    keras_models.cnn_model_9, keras_models.cnn_model_10, keras_models.cnn_model_11, keras_models.cnn_model_12]
+# Building 12 models
 
-for func in models:
+models = [keras_models.model_1, keras_models.model_2, keras_models.model_3, keras_models.model_4,
+    keras_models.model_5, keras_models.model_6, keras_models.model_7, keras_models.model_8,
+    keras_models.model_9, keras_models.model_10, keras_models.model_11, keras_models.model_12]
+
+for count, func in enumerate(models):
     model = func()
-    name = str(func.__name__)
+    name = str(func.__name__).split('_')[-1]
     print(f'TRAINING MODEL : {name}')
-    model_info[name] = keras_models.compile_and_train_model(model)['test_acc']
+    model_info[name] = keras_models.compile_and_train_model(model, count+1)['test_acc']
 
 # Building Traditional Models
-models = [trad_models.model_poly_SVM, trad_models.model_rbf_SVM, trad_models.model_DT, trad_models.model_RF]
+models = [trad_models.model_poly, trad_models.model_rbf, trad_models.model_DT, trad_models.model_RF]
 
 for func in models:
     model = func()
-    name = str(func.__name__)
+    name = str(func.__name__).split('_')[-1]
     print(f'TRAINING MODEL : {name}')
     model_info[name] = trad_models.model_score(model)
 
-labels = list(model_info.keys())
-values = list(model_info.values())
+labels = np.array(list(model_info.keys()))
+values = np.array(list(model_info.values()))
+
+sort_index = np.argsort(values)
+labels = labels[sort_index]
+values = values[sort_index]
 
 plt.figure()
 plt.bar(np.arange(len(labels)), values, align='center')
-plt.xticks(np.arange(len(labels)), labels, rotation='vertical')
+plt.xticks(np.arange(len(labels)), labels, rotation=40)
 plt.yticks(np.linspace(0, 1, 21))
 plt.savefig('model_comparison.png')
 plt.close()
